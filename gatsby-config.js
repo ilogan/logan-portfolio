8bae3ce2b309cea9564d0f18489bf3ba8581e51b
Ian Logan <itlogan9@gmail.com>
2020-04-15 16:54:04 -0500

    Add dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Montserrat Alternates\:400,700`, `Montserrat`, `Raleway`],
+        display: "swap"
+      }
+    }
+  ]
+};
