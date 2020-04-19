2eac0e84d71d10b8d4b5a558e6295fb2ed5d898a
Ian Logan <itlogan9@gmail.com>
2020-04-18 20:29:01 -0500

    Add home page to portfolio

--- src/components/containers/Dashboard.js
+++ src/components/containers/Dashboard.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Router } from "@reach/router";
 import { FeaturedProjectMock } from "../../utils/mockData";
+
 import FeaturedProject from "./Portfolio/FeaturedProject";
+import PortfolioHome from "./Portfolio/Home";
 
 function Dashboard(props) {
   return (
     <div {...props}>
       <Router basepath="portfolio">
         <FeaturedProject path=":id" {...FeaturedProjectMock} />
+        <PortfolioHome path="/" />
       </Router>
     </div>
   );
