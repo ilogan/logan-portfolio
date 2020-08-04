d7e9094a17dca422d9732df8760f8b5d0e5f7e45
Ian Logan <itlogan9@gmail.com>
2020-08-04 16:14:51 -0500

    Handle warnings

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,15 +1,10 @@
 import React from "react";
 
 import GlobalLayout from "../components/layouts/GlobalLayout";
-import Sidebar from "../components/PortfolioApp/Sidebar";
-import Dashboard from "../components/PortfolioApp/Dashboard";
 import Routes from "../components/PortfolioApp/Dashboard/Routes";
 
 export default () => (
   <GlobalLayout>
-    {/* <Sidebar/> */}
-    {/* <Dashboard> */}
     <Routes />
-    {/* </Dashboard> */}
   </GlobalLayout>
 );
