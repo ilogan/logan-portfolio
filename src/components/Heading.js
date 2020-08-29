b4ba2edfaff2c0725b563b48a1acfc5f5c70d36f
Ian Logan <itlogan9@gmail.com>
2020-08-29 18:46:30 -0500

    Span header full length of window

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { css } from "@emotion/core";
-import {contentWidth, flex} from "../utils/styleUtils"
+import { contentWidth, flex } from "../utils/styleUtils";
 
 function Heading({ title, children }) {
   return (
@@ -12,34 +12,38 @@ function Heading({ title, children }) {
       `}
     >
       <div
-        css={[contentWidth, flex, css`
-          flex-direction: column;
-          text-align: center;
-          background: #baebff;
-          padding: 50px 2.5rem;
-          border: 4px solid #1a202c;
-          border: 4px solid #3ea5ff;
-          border-radius: 8px;
-        `]}
+        css={[
+          flex,
+          css`
+            flex-direction: column;
+            text-align: center;
+            background: #baebff;
+            padding: 50px 0;
+            border-top: 4px solid #3ea5ff;
+            border-bottom: 4px solid #3ea5ff;
+          `
+        ]}
       >
-        <h1
-          css={css`
-            color: #3ea5ff;
-            font-size: 3rem;
-            font-weight: 700;
-            padding-bottom: 1rem;
-          `}
-        >
-          {title}
-        </h1>
-        <p
-          css={css`
-            font-weight: 600;
-            font-size: 1.5rem;
-          `}
-        >
-          {children}
-        </p>
+        <div css={contentWidth}>
+          <h1
+            css={css`
+              color: #3ea5ff;
+              font-size: 3rem;
+              font-weight: 700;
+              padding-bottom: 1rem;
+            `}
+          >
+            {title}
+          </h1>
+          <p
+            css={css`
+              font-weight: 600;
+              font-size: 1.5rem;
+            `}
+          >
+            {children}
+          </p>
+        </div>
       </div>
     </div>
   );
