1f8ad6afa01dde7e485a7767b7af54d1b4c7e58d
Ian Logan <itlogan9@gmail.com>
2020-08-31 21:56:37 -0500

    Flatten tags

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -8,9 +8,9 @@ function Tag({ color, backgroundColor, children }) {
   return (
     <div
       css={css`
-        border: 3px solid white;
+        border: 4px solid white;
         font-weight: 600;
-        box-shadow: 0 3px 8px rgba(0, 0, 0, 0.05);
+        /* box-shadow: 0 3px 8px rgba(0, 0, 0, 0.15); */
         color: ${color || defaultDark};
         background-color: ${backgroundColor || defaultLight};
         padding: 1px 8px;
