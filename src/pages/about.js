04355eb1a30b073ef1a0e89c673e3edecbd0e7e2
Ian Logan <itlogan9@gmail.com>
2020-09-17 14:57:02 -0500

    Change about heading copy

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,9 +6,8 @@ import { About } from "../components/About";
 export default () => (
   <GlobalLayout>
     <Heading title="About" backgroundColor="#ff9ae9">
-      I hold a B.S. in Computer Science from Loyola University Chicago, and I
-      recently graduated from Fullstack Academy's Software Engineering Immersive
-      Program.
+      I hold a B.S. in Computer Science from Loyola University Chicago, and I am
+      a graduate of Fullstack Academy's Software Engineering Immersive Program.
     </Heading>
     <About />
   </GlobalLayout>
