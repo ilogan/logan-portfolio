927b7eec02f24b6ff749cec7f9d31b1893167f75
Ian Logan <itlogan9@gmail.com>
2020-09-17 14:25:33 -0500

    Write initial text for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,9 +5,10 @@ import { About } from "../components/About";
 
 export default () => (
   <GlobalLayout>
-    <Heading title="About Me" backgroundColor="#ff9ae9">
-      I hold a B.S. in Computer Science, but there's a lot more to my story than
-      that.
+    <Heading title="About" backgroundColor="#ff9ae9">
+      I hold a B.S. in Computer Science from Loyola University Chicago, and I
+      recently graduated from Fullstack Academy's Software Engineering Immersive
+      Program.
     </Heading>
     <About />
   </GlobalLayout>
