c48f7069c6c8bcbe71ae6caeda085c740e167b51
Ian Logan <itlogan9@gmail.com>
2020-09-18 10:15:01 -0500

    Cleanup redundant/unused markup

--- src/components/PortfolioApp/SingleProject.js
+++ src/components/PortfolioApp/SingleProject.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { css } from "@emotion/core";
 import Image from "gatsby-image";
-import { flex, bp } from "../../utils/styleUtils";
+import { bp } from "../../utils/styleUtils";
 import LinkIcon from "../../../assets/link.svg";
 import Tag from "../Tag";
 import { ButtonLink } from "../Button";
@@ -27,231 +27,171 @@ function SingleProject({ project, number, setSection }) {
     <section id={project.title} ref={sectionRef}>
       <div
         css={css`
-          position: absolute;
-          width: 100vw;
-          height: 300px;
-          z-index: -1;
-          left: 0;
+          width: 100%;
+          margin-top: 24px;
+          margin-bottom: 16px;
           @media (min-width: ${bp.md}) {
-            height: 364px;
-          }
-        `}
-      ></div>
-      <div
-        css={css`
-          padding-top: 24px;
-          padding-bottom: 182px;
-          @media (min-width: ${bp.md}) {
-            padding-top: 60px;
+            margin-top: 60px;
           }
         `}
       >
-        <div css={[flex]}>
-          <div
-            css={css`
-              width: 100%;
-            `}
-          >
-            <div
-              css={css`
-                text-align: right;
-                margin-bottom: 0.5rem;
-                color: #1a202c;
-                font-size: 1rem;
-              `}
-            ></div>
-            <div
-              css={css`
-                padding-bottom: 12px;
-                /* border-bottom: 8px solid #a2a2a2; */
-              `}
-            >
-              <div
-                css={css`
-                  margin-bottom: 0.75rem;
-                  @media (min-width: ${bp.sm}) {
-                    display: flex;
-                  }
-                `}
-              >
-                <ProjectNumber number={number} />
-                <h2
-                  css={css`
-                    text-align: center;
-                    @media (min-width: ${bp.sm}) {
-                      text-align: left;
-                    }
-                  `}
-                >
-                  {project.title}
-                </h2>
-              </div>
-              {!project.siteUrl ? null : (
-                <a
-                  css={css`
-                    display: flex;
-                    align-items: center;
-                    width: fit-content;
-                    font-size: 0.825rem;
-                    color: #343c4a;
-                    text-decoration: none;
-                    margin: 0 auto;
-
-                    @media (min-width: ${bp.sm}) {
-                      margin: 0;
-                      transform: translateX(75px);
-                      font-size: 1rem;
-                    }
-                    @media (min-width: ${bp.md}) {
-                      transform: translateX(96px);
-                      font-size: 1.125rem;
-                    }
-                  `}
-                  href={project.siteUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <LinkIcon
-                    height="1rem"
-                    css={css`
-                      fill: #343c4a;
-                      padding-right: 0.5rem;
-                    `}
-                  />
-                  {project.siteUrl}
-                </a>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <div
-        css={css`
-          background-color: #cbcbcb;
-          height: 400px;
-        `}
-      ></div> */}
-      <div
-        css={[
-          flex,
-          css`
-            margin-top: -170px;
-          `
-        ]}
-      >
-        {/* <aside
-          css={css`
-            min-width: 7.25rem;
-            margin-right: 5rem;
-            text-align: right;
-          `}
-        >
-        </aside> */}
         <div
           css={css`
-            width: 100%;
+            margin-bottom: 12px;
+            @media (min-width: ${bp.sm}) {
+              display: flex;
+            }
           `}
         >
-          <div
+          <ProjectNumber number={number} />
+          <h2
+            css={css`
+              text-align: center;
+              @media (min-width: ${bp.sm}) {
+                text-align: left;
+              }
+            `}
+          >
+            {project.title}
+          </h2>
+        </div>
+        {!project.siteUrl ? null : (
+          <a
             css={css`
               display: flex;
-              margin-bottom: 32px;
+              align-items: center;
+              width: fit-content;
+              font-size: 0.825rem;
+              color: #343c4a;
+              text-decoration: none;
+              margin: 0 auto;
+
+              @media (min-width: ${bp.sm}) {
+                margin: 0;
+                transform: translateX(75px);
+                font-size: 1rem;
+              }
+              @media (min-width: ${bp.md}) {
+                transform: translateX(96px);
+                font-size: 1.125rem;
+              }
             `}
+            href={project.siteUrl}
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <Image
-              alt="project"
-              fluid={project.fluid}
+            <LinkIcon
+              height="1rem"
               css={css`
-                width: 100%;
-                border-radius: 8px;
-                border: 1px solid #1a202c;
-                box-shadow: 12px 12px #1a202c;
-                /* @media (min-width: ${bp.sm}) {
-                  border-width: 8px;
-                } */
+                fill: #343c4a;
+                padding-right: 0.5rem;
               `}
             />
-          </div>
-          <div
+            {project.siteUrl}
+          </a>
+        )}
+      </div>
+
+      <div
+        css={css`
+          width: 100%;
+        `}
+      >
+        <div
+          css={css`
+            margin-bottom: 32px;
+          `}
+        >
+          <Image
+            alt="project"
+            fluid={project.fluid}
             css={css`
-              margin-bottom: 4rem;
+              width: 100%;
+              border-radius: 8px;
+              border: 1px solid #1a202c;
+              box-shadow: 12px 12px #1a202c;
             `}
-          >
-            <ul
-              css={css`
-                display: flex;
-                justify-content: center;
-                flex-wrap: wrap;
-                margin-bottom: 1.5rem;
-                list-style: none;
-                padding-left: 0;
-                > li {
-                  font-size: 0.8125rem;
-                  margin-right: 1rem;
-                  margin-bottom: 0.25rem;
-                  &:last-child {
-                    margin-right: 0;
-                  }
+          />
+        </div>
+        <div
+          css={css`
+            margin-bottom: 4rem;
+          `}
+        >
+          <ul
+            css={css`
+              display: flex;
+              justify-content: center;
+              flex-wrap: wrap;
+              margin-bottom: 1.5rem;
+              list-style: none;
+              padding-left: 0;
+              > li {
+                font-size: 0.8125rem;
+                margin-right: 1rem;
+                margin-bottom: 0.25rem;
+                &:last-child {
+                  margin-right: 0;
                 }
-              `}
-            >
-              {project.tags.data.map(t => (
-                <li key={t._id}>
-                  <Tag
-                    color={t.type.color}
-                    backgroundColor={t.type.backgroundColor}
-                  >
-                    {t.name}
-                  </Tag>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div
-            css={css`
-              margin-bottom: 2rem;
+              }
             `}
           >
-            <h3>Overview</h3>
-            <p>{project.summary}</p>
-          </div>
-          <div
-            css={css`
-              margin-bottom: 2rem;
-            `}
-          >
-            <h3>Observations</h3>
-            {project.responsibilities.map(r => (
-              <p key={r.text}>{r.text}</p>
+            {project.tags.data.map(t => (
+              <li key={t._id}>
+                <Tag
+                  color={t.type.color}
+                  backgroundColor={t.type.backgroundColor}
+                >
+                  {t.name}
+                </Tag>
+              </li>
             ))}
-          </div>
-          <div
+          </ul>
+        </div>
+        <div
+          css={css`
+            margin-bottom: 2rem;
+          `}
+        >
+          <h3>Overview</h3>
+          <p>{project.summary}</p>
+        </div>
+        <div
+          css={css`
+            margin-bottom: 2rem;
+          `}
+        >
+          <h3>Observations</h3>
+          {project.responsibilities.map(r => (
+            <p key={r.text}>{r.text}</p>
+          ))}
+        </div>
+        <div
+          css={css`
+            margin-bottom: 2rem;
+          `}
+        >
+          <h3>Technical Features</h3>
+          <ul
             css={css`
-              margin-bottom: 2rem;
+              > li {
+                margin-bottom: 0.5rem;
+              }
             `}
           >
-            <h3>Technical Features</h3>
-            <ul
-              css={css`
-                > li {
-                  margin-bottom: 0.5rem;
-                }
-              `}
-            >
-              {project.technicalFeatures.map(f => (
-                <li key={f.text}>{f.text}</li>
-              ))}
-            </ul>
-          </div>
-          <div
-            css={css`
-              padding-bottom: 96px;
-            `}
-          >
-            <ButtonLink href={project.githubUrl} dark>
-              check it out on GitHub
-            </ButtonLink>
-          </div>
+            {project.technicalFeatures.map(f => (
+              <li key={f.text}>{f.text}</li>
+            ))}
+          </ul>
+        </div>
+        <div
+          css={css`
+            padding-bottom: 96px;
+          `}
+        >
+          <ButtonLink href={project.githubUrl} dark>
+            check it out on GitHub
+          </ButtonLink>
         </div>
       </div>
     </section>
