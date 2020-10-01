e06b5419d1b002eb46566863653c05667ecd0f7a
Ian Logan <itlogan9@gmail.com>
2020-10-01 06:16:59 -0500

    Insert dynamic data into posts

---
+++ src/templates/post.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { css } from "@emotion/core";
+import { Link, graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import { contentWidth } from "../utils/styleUtils";
+
+import GlobalLayout from "../components/layouts/GlobalLayout";
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        author
+      }
+      body
+    }
+  }
+`;
+
+function PostTemplate({ data: { mdx: post } }) {
+  console.log(post);
+  return (
+    <GlobalLayout>
+      <section
+        css={css`
+          ${contentWidth}
+          margin-top: 64px;
+        `}
+      >
+        <article>
+          <h1
+            css={css`
+              font-size: 3rem;
+            `}
+          >
+            {post.frontmatter.title}
+          </h1>
+          <MDXRenderer>{post.body}</MDXRenderer>
+          <Link to="/blog/">&larr; Back</Link>
+        </article>
+      </section>
+    </GlobalLayout>
+  );
+}
+
+export default PostTemplate;
